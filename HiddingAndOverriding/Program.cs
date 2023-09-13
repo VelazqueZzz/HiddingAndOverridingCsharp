c49b6cc158985b29dc9afda5f890ec48ca3004c5
ITaly177 Milan <patrokl2010@yandex.ru>
2023-09-13 15:29:59 +0300

    Add project files.

---
+++ HiddingAndOverriding/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Alpha
+{
+    public void hello()
+    {
+        Console.WriteLine("Metod hello() iz classa Alpha");
+    }
+    public virtual void hi()
+    {
+        Console.WriteLine("metod Hi() iz classa Alpha");
+    }
+        public void show()
+        {
+            hello();
+            hi();
+            Console.WriteLine();
+
+        }
+    }
+class Bravo: Alpha
+{
+    new public void hello()
+    {
+        Console.WriteLine("Metod hello() iz classa Bravo()");
+    }
+    public override void hi()
+    {
+         Console.WriteLine("Metod hi() iz classa Bravo");
+        }
+    }
+class HiddinfAndOverridingDemo
+{
+    static void Main()
+    {
+        Alpha A = new Alpha();
+        Console.WriteLine("Vipolnenie commandi A.Show():");
+        A.show();
+        Bravo B = new Bravo();
+        Console.WriteLine("Vipolnenie commandi B.hello():");
+        B.hello();
+        Console.WriteLine("Vipolnenie commandi B.hi():");
+        B.hi();
+        Console.WriteLine("Vipolnenie commandi B.Show():");
+        B.show();
+        Console.WriteLine("Vipolnenie commandi A=B:");
+        A = B;
+        A.hello();
+        Console.WriteLine("Vipolnenie commandi A.hi():");
+        A.hi();
+        Console.WriteLine("Vipolnenie commandi A.Show():");
+        A.show();
+    }
+}
+
